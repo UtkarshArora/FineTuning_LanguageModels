87ce8eef9e15809416958524b4842c529796493e
Utkarsh Arora <ua2152@cs001.hpc.nyu.edu>
2025-11-18 00:57:14 -0500

    changed num_epochs in train_t5

--- hw4-code/part-2-code/train_t5.py
+++ hw4-code/part-2-code/train_t5.py
@@ -58,7 +58,7 @@ def get_args():
     parser.add_argument(
         "--max_n_epochs",
         type=int,
-        default=0,
+        default=10,
         help="How many epochs to train the model for",
     )
     parser.add_argument(
